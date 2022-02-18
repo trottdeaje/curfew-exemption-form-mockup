a7e5f5c85b8bf7ffb2e3aedc3612d380fa3a10e2
DeajeT <deajetrott@gmail.com>
2022-02-18 15:17:45 +0000

    Disabled banner resize on scroll

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.onscroll = function() {scrollFunction()}
+// window.onscroll = function() {scrollFunction()}
 
-function scrollFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("header-inner").style.padding = "0.7em 1.5em";
-        document.getElementById('logo').style.width = "1.8em"
-        document.getElementById('header').className = "govbm-header header--shadow"
-    } else {
-        document.getElementById("header-inner").style.padding = "";
-        document.getElementById('logo').style.width = ""
-        document.getElementById('header').className = "govbm-header"
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//         document.getElementById("header-inner").style.padding = "0.7em 1.5em";
+//         document.getElementById('logo').style.width = "1.8em"
+//         document.getElementById('header').className = "govbm-header header--shadow"
+//     } else {
+//         document.getElementById("header-inner").style.padding = "";
+//         document.getElementById('logo').style.width = ""
+//         document.getElementById('header').className = "govbm-header"
+//     }
+// }
